311a68fb03cd7be282585379e8955f84f04319fe
xyym <1920376753@qq.com>
2022-12-06 00:32:40 +0800

    12345

--- pygit/main.py
+++ pygit/main.py
@@ -1,10 +1,19 @@
-import re
+
 from git import Repo
 import os
 
 
 
-repo = Repo.init(r"D:\Repositories\pygit-test-repo")
-# git = repo.git
-# git.add(".")
-# git.commit("-m", "add some file")
+repo = Repo.init(r"C:\Repositories\test-repo")
+git = repo.git
+git.add(".")
+remote = repo.remote() #通过repo对象获取remote对象
+if repo.is_dirty():
+    print("dirty")
+    git.commit("-m", "add some file")
+    remote.push()
+else:
+    print("not dirty")
+remote.pull()
+
+print(git.log())
