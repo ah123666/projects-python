6560f3311433095d74a0355d5e268caccf61d3c3
xyym <1920376753@qq.com>
2022-12-05 22:48:37 +0800

    123

---
+++ pygit/main.py
@@ -0,0 +1,11 @@
+import re
+from git import Repo
+import os
+
+
+
+# repo = Repo.init(r"D:\Repositories\projects-python\testpygit")
+repo = Repo(r"D:\Repositories\projects-python\testpygit")
+git = repo.git
+git.add(".")
+git.commit("-m", "add some file")
