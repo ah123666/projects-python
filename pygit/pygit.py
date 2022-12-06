0934493d5492ec2a6531e57f4ae4464905a08db2
xyym <1920376753@qq.com>
2022-12-06 17:32:46 +0800

    commit at 2022-12-06 17:32:45

--- pygit/pygit.py
+++ pygit/pygit.py
@@ -4,8 +4,6 @@ from git import Repo, InvalidGitRepositoryError
 import os
 
 
-
-
 def getAllDir(rootDir):
     """
     返回当前目录下的所有文件夹
@@ -25,15 +23,24 @@ def pullAll(rootDir):
     """
     dirList = getAllDir(rootDir)
     for dir in dirList:
+        print("=" * 100)
         print("current dir: {}".format(dir))
         try:
             repo = Repo(dir)
         except InvalidGitRepositoryError:
             print("current dir is not a git repo!")
         else:
-            remote = repo.remote()
-            info = remote.pull()
-            print(info)
+            try:
+                remote = repo.remote(name="github")
+                print("remote_url: ", remote.url)
+                info = remote.pull()[0]
+                print("flags: ", info.flags)
+                print("ref: ", info.ref)
+                print("note: ", info.note)
+                print("old_commit: ", info.old_commit)
+                print("remote_ref_path: ", info.remote_ref_path)
+            except:
+                print("no remote named github")
 
 
 def pushAll(rootDir):
@@ -42,6 +49,7 @@ def pushAll(rootDir):
     """
     dirList = getAllDir(rootDir)
     for dir in dirList:
+        print("=" * 100)
         print("current dir: {}".format(dir))
         try:
             repo = Repo(dir)
@@ -52,15 +60,26 @@ def pushAll(rootDir):
             git.add(".")
             if repo.is_dirty():
                 git.commit("-m", datetime.datetime.now().strftime("commit at %Y-%m-%d %H:%M:%S"))
-                remote = repo.remote()
-                info = remote.push()
-                print(info)
+                try:
+                    remote = repo.remote(name="github")
+                    print("remote_url: ", remote.url)
+                    info = remote.push()[0]
+                    print("flags: ", info.flags)
+                    print("local_ref: ", info.local_ref)
+                    print("remote_ref_string: ", info.remote_ref_string)
+                    print("remote_ref: ", info.remote_ref)
+                    print("old_commit: ", info.old_commit)
+                    print("summary: ", info.summary)
+                except:
+                    print("no remote named github")
             else:
                 print("nothing to commit")
 
 
 
 if __name__ == "__main__":
-    rootDir = os.path.dirname(__file__)
+    # rootDir = os.path.dirname(__file__)
+    rootDir = r"D:\Repositories"
     print("root dir: {}".format(rootDir))
     pushAll(rootDir)
+    # pullAll(rootDir)
