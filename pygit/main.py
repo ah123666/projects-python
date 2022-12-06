4357773751d9a8e854513edbca4c4b4e7988b26d
xyym <1920376753@qq.com>
2022-12-06 11:38:19 +0800

    commit at 2022-12-06 11:38:19

--- pygit/main.py
+++ pygit/main.py
@@ -1,19 +1,66 @@
 
-from git import Repo
+import datetime
+from git import Repo, InvalidGitRepositoryError
 import os
 
 
 
-repo = Repo.init(r"C:\Repositories\test-repo")
-git = repo.git
-git.add(".")
-remote = repo.remote() #通过repo对象获取remote对象
-if repo.is_dirty():
-    print("dirty")
-    git.commit("-m", "add some file")
-    remote.push()
-else:
-    print("not dirty")
-remote.pull()
 
-print(git.log())
+def getAllDir(rootDir):
+    """
+    返回当前目录下的所有文件夹
+    """
+    dirList = []
+    allFile = os.listdir(rootDir)
+    for file in allFile:
+        curPath = os.path.join(rootDir, file)
+        if os.path.isdir(curPath):
+            dirList.append(curPath)
+    return dirList
+
+
+def pullAll(rootDir):
+    """
+    将当前目录下的所有git仓库拉取远程仓库
+    """
+    dirList = getAllDir(rootDir)
+    for dir in dirList:
+        print("current dir: {}".format(dir))
+        try:
+            repo = Repo(dir)
+        except InvalidGitRepositoryError:
+            print("current dir is not a git repo!")
+        else:
+            remote = repo.remote()
+            info = remote.pull()
+            print(info)
+
+
+def pushAll(rootDir):
+    """
+    将当前目录下的所有git仓库进行添加所有文件、提交、推送远程仓库
+    """
+    dirList = getAllDir(rootDir)
+    for dir in dirList:
+        print("current dir: {}".format(dir))
+        try:
+            repo = Repo(dir)
+        except InvalidGitRepositoryError:
+            print("current dir is not a git repo!")
+        else:
+            git = repo.git
+            git.add(".")
+            if repo.is_dirty():
+                git.commit("-m", datetime.datetime.now().strftime("commit at %Y-%m-%d %H:%M:%S"))
+                remote = repo.remote()
+                info = remote.push()
+                print(info)
+            else:
+                print("nothing to commit")
+
+
+
+if __name__ == "__main__":
+    rootDir = os.path.dirname(__file__)
+    print("root dir: {}".format(rootDir))
+    pushAll(rootDir)
